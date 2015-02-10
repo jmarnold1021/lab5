2a2594e7a7be2e517ca2c14e33b42a73c1bb0f82
vagrant <vagrant@precise32.(none)>
2015-02-10 19:44:32 +0000

    added query.on(end) event handler instead of row to index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var cars={'cars':[]};
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   var query = client.query('SELECT * FROM test_tab');
 
-  query.on('row', function(row) {
+  query.on('end', function(row) {
     console.log(JSON.stringify(row));
 		cars['cars'].push(row);
   });
