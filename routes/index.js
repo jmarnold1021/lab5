71f7156ffe51eb2d49675f9e6e107f5cc9b42a5b
vagrant <vagrant@precise32.(none)>
2015-02-10 20:12:33 +0000

    moved connection inside export call

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,24 @@
 //var data = require('../data.json');
 var pg = require('pg');
 
-var cars={'cars':[]};
-
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  var query = client.query('SELECT * FROM test_tab');
-
-  query.on('row', function(row,result) {
-    console.log(JSON.stringify(row));
-		cars['cars'].push(row);
-  });
-	query.on('end',function(result){
-     console.log(cars);  
-		 client.end();
-
-	});					 
-	  
-});
 
 exports.view = function(req, res){
+
+  var cars={'cars':[]};
+
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+    var query = client.query('SELECT * FROM test_tab');
+
+    query.on('row', function(row,result) {
+      console.log(JSON.stringify(row));
+		  cars['cars'].push(row);
+    });
+	  query.on('end',function(result){
+		  client.end();
+
+	  });					 
+  });
+
 
 
 	console.log(cars);
