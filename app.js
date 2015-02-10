8cc32c29280d192d497bcdc0ced9e1265728e0ee
vagrant <vagrant@precise32.(none)>
2015-02-10 18:48:57 +0000

    moved db connect and access to add.js and added html in handlebars

--- app.js
+++ app.js
@@ -49,10 +49,4 @@ http.createServer(app).listen(app.get('port'), function(){
 
 
 
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  var query = client.query('SELECT * FROM test_tab');
 
-  query.on('row', function(row) {
-    console.log(JSON.stringify(row));
-  });
-});
--- routes/add.js
+++ routes/add.js
@@ -1,7 +1,17 @@
 var data = require("../data.json");
+var cars={'cars':[]};
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  var query = client.query('SELECT * FROM test_tab');
+
+  query.on('row', function(row) {
+    console.log(JSON.stringify(row));
+		cars['cars'].push(row);
+  });
+});
+
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
 	data["friends"].push({name:req.query.name,description:req.query.description,imageUrl:"http://lorempixel.com/400/400/people"});
-	res.render('add',data);
+	res.render('add',data,cars);
  }
