1f398a9a456fa42cf37d8896aa645b200adba307
vagrant <vagrant@precise32.(none)>
2015-02-10 19:36:46 +0000

    req scope Bug in add.js

--- routes/add.js
+++ routes/add.js
@@ -1,26 +1,31 @@
 //var data = require("../data.json");
 var pg = require('pg');
 
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  var query = client.query('Insert Into test_tab(name,car) values($1,$2) RETURNING name',[req.query.name,req.query.car],
-	                          function(err,result){
 
-														  if(err){
-                               console.log(err);
-															}
-															else {
-                                console.log('row inserted with id: ' + result.rows[0].name);
-                              }
-															client.end();
+
+
+
+exports.addFriend = function(req, res) { 
+
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+    var query = client.query('Insert Into test_tab(name,car) values($1,$2) RETURNING name',[req.query.name,req.query.car],
+	                            function(err,result){
+                                if(err){
+                                  console.log(err);
+															  }
+															  else {
+                                  console.log('row inserted with id: ' + result.rows[0].name);
+                                }
+															 client.end();
 													   });
 														 
 
     
-});
+  });  
 
 
 
-exports.addFriend = function(req, res) {    
+
 	// Your code goes here
 	//data["friends"].push({name:req.query.name,description:req.query.description,imageUrl:"http://lorempixel.com/400/400/people"});
 	res.render('add');
